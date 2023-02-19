13350f17b1635dc4b8b59a95994794f322169008
Joshua Kinder <97398293+joshjkk@users.noreply.github.com>
2023-02-19 21:57:57 +0000

    Change theme

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -18,10 +18,58 @@ return require('packer').startup(function(use)
 	  as = 'rose-pine',
 	  config = function()
 		  require("rose-pine").setup()
-		  vim.cmd('colorscheme rose-pine')
+		  -- vim.cmd('colorscheme rose-pine')
 	  end
   })
-				
+
+  use { "catppuccin/nvim", as = "catppuccin" }
+
+  require("catppuccin").setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+    },
+    transparent_background = false,
+    show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+    term_colors = false,
+    dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        notify = false,
+        mini = false,
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+})
+
+  -- setup must be called before loading
+  vim.cmd.colorscheme "catppuccin"
+
   -- use('Mofiqul/vscode.nvim')
 
   -- vim.cmd('colorscheme vscode')
