01605851b0909949b064bc5c76e3e0e3858282c6
Joshua Kinder <97398293+joshjkk@users.noreply.github.com>
2023-02-23 21:10:59 +0000

    Fix 4 spaces indents

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -2,10 +2,11 @@ vim.opt.guicursor = ""
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.shiftwidth = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
