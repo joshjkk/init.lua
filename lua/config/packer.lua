38a4a6f9260faaebd288f6c3b675dba4188aac42
Joshua Kinder <97398293+joshjkk@users.noreply.github.com>
2023-03-10 11:53:40 +0000

    Update packer.lua

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -64,7 +64,7 @@ return require('packer').startup(function(use)
                   StatusLine = { fg = 'love', bg = 'love', blend = 10 },
               }
           })
-		  -- vim.cmd('colorscheme rose-pine')
+		  vim.cmd('colorscheme rose-pine')
 	  end
   })
 
@@ -76,57 +76,7 @@ return require('packer').startup(function(use)
     tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
 
-  use { "catppuccin/nvim", as = "catppuccin" }
-
-  require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-    },
-    transparent_background = false,
-    show_end_of_buffer = false, -- show the '~' characters after the end of buffers
-    term_colors = false,
-    dim_inactive = {
-        enabled = false,
-        shade = "dark",
-        percentage = 0.15,
-    },
-    no_italic = false, -- Force no italic
-    no_bold = false, -- Force no bold
-    styles = {
-        comments = { "italic" },
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-    },
-    color_overrides = {},
-    custom_highlights = {},
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        telescope = true,
-        notify = false,
-        mini = false,
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    },
-  })
-
-  -- setup must be called before loading
-  -- vim.cmd.colorscheme "catppuccin"
-
   use { "ellisonleao/gruvbox.nvim" }
-
-  vim.cmd.colorscheme("rose-pine")
 
   use {
       'nvim-lualine/lualine.nvim',
