79b84c6849525951f66bf9f6681560b252018daa
Joshua Kinder <97398293+joshjkk@users.noreply.github.com>
2023-03-10 10:24:54 +0000

    Update remap.lua

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -4,3 +4,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
   {silent = true, noremap = true}
 )
+
+vim.keymap.set("n", "<leader>n", "<cmd>NvimTreeToggle<cr>",
+  {silent = true, noremap = true}
+)
